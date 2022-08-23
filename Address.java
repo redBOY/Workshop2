cdac2e5164f544c0b251657678237036aae7d10f
“Adithya <adithyasrinivas66@gmail.com>
2022-08-23 20:54:50 +0530

    Address commit

---
+++ Address.java
@@ -0,0 +1,25 @@
+package main.main.Workshop2;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Address {
+    void add_contacts() {
+
+            ArrayList<Contacts> ContactsList = new ArrayList<>();
+            Scanner scan = new Scanner(System.in);
+            System.out.println("enter name");
+            String name = scan.next();
+            System.out.println("enter age");
+            int age = scan.nextInt();
+            System.out.println("enter phoneNumber");
+            String phoneNumber = scan.next();
+            System.out.println("enter city");
+            String city = scan.next();
+            System.out.println("enter state");
+            String state = scan.next();
+            ContactsList.add(Contacts);
+        }
+    }
+
+
